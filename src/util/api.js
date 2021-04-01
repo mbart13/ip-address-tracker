1e90453931798ac39d9fe290d09920dad6b14b2e
Michal Bartosik <msbartosik@gmail.com>
2021-04-01 15:24:57 +0200

    remove console logs

--- src/util/api.js
+++ src/util/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const { REACT_APP_API_KEY } = process.env;
 const baseUrl = `https://geo.ipify.org/api/v1?apiKey=${REACT_APP_API_KEY}`
-console.log(baseUrl);
+
 const getIPData = async(domain='') => {
   const request = await axios.get(`${baseUrl}&domain=${domain}`)
   return request.data
