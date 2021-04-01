2ad639bd8dece12b8a7a4023ec023577ef531923
Michal Bartosik <msbartosik@gmail.com>
2021-04-01 15:18:34 +0200

    testing env variables

--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'https://geo.ipify.org/api/v1?apiKey=at_XaLMcw0hXDs75kIMjgNzNCfQiQ4IJ'
 
+const { REACT_APP_API_KEY } = process.env;
+const baseUrl = `https://geo.ipify.org/api/v1?apiKey=${REACT_APP_API_KEY}`
+console.log(baseUrl);
 const getIPData = async(domain='') => {
   const request = await axios.get(`${baseUrl}&domain=${domain}`)
   return request.data
